915949f169c343c899ce6bb5b787759804388851
TArtikainen <57004957+TArtikainen@users.noreply.github.com>
2019-11-29 18:10:28 +0200

    Päivitetty tietokanta

--- liittymat.sql
+++ liittymat.sql
@@ -5,7 +5,7 @@
 -- Dumped from database version 12.0
 -- Dumped by pg_dump version 12.0
 
--- Started on 2019-11-29 14:14:08
+-- Started on 2019-11-29 18:08:20
 
 SET statement_timeout = 0;
 SET lock_timeout = 0;
@@ -38,7 +38,7 @@ CREATE TABLE public.liittyma (
 ALTER TABLE public.liittyma OWNER TO postgres;
 
 --
--- TOC entry 203 (class 1259 OID 16736)
+-- TOC entry 205 (class 1259 OID 24903)
 -- Name: perusliittymat; Type: TABLE; Schema: public; Owner: postgres
 --
 
@@ -53,7 +53,7 @@ CREATE TABLE public.perusliittymat (
 ALTER TABLE public.perusliittymat OWNER TO postgres;
 
 --
--- TOC entry 204 (class 1259 OID 16741)
+-- TOC entry 203 (class 1259 OID 16741)
 -- Name: prepaidliittymat; Type: TABLE; Schema: public; Owner: postgres
 --
 
@@ -68,7 +68,7 @@ CREATE TABLE public.prepaidliittymat (
 ALTER TABLE public.prepaidliittymat OWNER TO postgres;
 
 --
--- TOC entry 205 (class 1259 OID 16747)
+-- TOC entry 204 (class 1259 OID 24893)
 -- Name: yritysliittymat; Type: TABLE; Schema: public; Owner: postgres
 --
 
@@ -93,23 +93,22 @@ COPY public.liittyma (puhelinnumero, operaattori, datanopeus, hinta) FROM stdin;
 
 
 --
--- TOC entry 2830 (class 0 OID 16736)
--- Dependencies: 203
+-- TOC entry 2832 (class 0 OID 24903)
+-- Dependencies: 205
 -- Data for Name: perusliittymat; Type: TABLE DATA; Schema: public; Owner: postgres
 --
 
 COPY public.perusliittymat (puhelinnumero, operaattori, datanopeus, hinta) FROM stdin;
-223853	telia	100	10
-791931	DNA	250	50
-0	telia	100	100
-451420	elisa	250	50
-297980	elisa	100	10
+350288	Telia	100	18
+581830	Elisa	150	23
+386948	DNA	250	29
+549322	Telia	200	25
 \.
 
 
 --
--- TOC entry 2831 (class 0 OID 16741)
--- Dependencies: 204
+-- TOC entry 2830 (class 0 OID 16741)
+-- Dependencies: 203
 -- Data for Name: prepaidliittymat; Type: TABLE DATA; Schema: public; Owner: postgres
 --
 
@@ -117,31 +116,26 @@ COPY public.prepaidliittymat (puhelinnumero, operaattori, datanopeus, hinta) FRO
 172766	dna	50	10
 118463	telia	100	19
 315671	moi	200	20
+753210	Elisa	100	17
 \.
 
 
 --
--- TOC entry 2832 (class 0 OID 16747)
--- Dependencies: 205
+-- TOC entry 2831 (class 0 OID 24893)
+-- Dependencies: 204
 -- Data for Name: yritysliittymat; Type: TABLE DATA; Schema: public; Owner: postgres
 --
 
 COPY public.yritysliittymat (puhelinnumero, operaattori, datanopeus, hinta) FROM stdin;
-769620	ewf	34	32
-106970	elisa	100	10
-750638	DNA	200	20
-120734	elisa	200	20
-667637	elisa	300	25
-7	Dna	100	50
-518124	moi	100	20
-677123	telia	100	10
-949757	telia	300	20
-839388	telia	300	20
+931112	Telia	200	25
+314743	Elisa	250	32
+480966	DNA	150	20
+104221	Telia	100	15
 \.
 
 
 --
--- TOC entry 2698 (class 2606 OID 16740)
+-- TOC entry 2702 (class 2606 OID 24907)
 -- Name: perusliittymat perusliittymat_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
 --
 
@@ -150,7 +144,7 @@ ALTER TABLE ONLY public.perusliittymat
 
 
 --
--- TOC entry 2700 (class 2606 OID 16745)
+-- TOC entry 2698 (class 2606 OID 16745)
 -- Name: prepaidliittymat prepaidliittymat_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
 --
 
@@ -159,7 +153,7 @@ ALTER TABLE ONLY public.prepaidliittymat
 
 
 --
--- TOC entry 2702 (class 2606 OID 16751)
+-- TOC entry 2700 (class 2606 OID 24897)
 -- Name: yritysliittymat yritysliittymat_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
 --
 
@@ -167,7 +161,7 @@ ALTER TABLE ONLY public.yritysliittymat
     ADD CONSTRAINT yritysliittymat_pkey PRIMARY KEY (puhelinnumero);
 
 
--- Completed on 2019-11-29 14:14:09
+-- Completed on 2019-11-29 18:08:21
 
 --
 -- PostgreSQL database dump complete
